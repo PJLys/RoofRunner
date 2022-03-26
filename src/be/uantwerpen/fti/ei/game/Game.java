5b051ce890282dd0640d7e3348c3f774bac7fb5a
Rudydevadder <67599688+Rudydevadder@users.noreply.github.com>
2022-03-26 16:10:47 +0100

    Game Class:
    This will contain the functionalities of the game but is quite empty at the moment.

---
+++ src/be/uantwerpen/fti/ei/game/Game.java
@@ -0,0 +1,14 @@
+package be.uantwerpen.fti.ei.game;
+
+import be.uantwerpen.fti.ei.GraphicsCTX;
+
+public class Game {
+    public Game(GraphicsCTX gctx){
+    }
+    public void run(){
+        build();
+    }
+    private void build(){
+
+    }
+}
