6f38b23ba53e946c499f6b76fb1f658bb5316d33
Rudydevadder <67599688+Rudydevadder@users.noreply.github.com>
2022-03-26 16:02:57 +0100

    Adding a movement component: - floating point: This is a platform game, so all the movable objects have to be able to move freely from the grid

---
+++ src/be/uantwerpen/fti/ei/components/Cmovement.java
@@ -0,0 +1,10 @@
+package be.uantwerpen.fti.ei.components;
+
+
+public class Cmovement {
+    private float x;
+    private float y;
+    private float dx;
+    private float dy;
+    private boolean[] collisionflags;
+}
