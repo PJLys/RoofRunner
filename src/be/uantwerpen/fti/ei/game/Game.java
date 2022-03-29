4d35ad28455d6b79c4b1bd41a95c30408a1ae4ac
PJLys <67599688+Rudydevadder@users.noreply.github.com>
2022-03-29 09:43:57 +0200

    Drawing test Collectables
    Example positions of collectable objects will be painted

--- src/be/uantwerpen/fti/ei/game/Game.java
+++ src/be/uantwerpen/fti/ei/game/Game.java
@@ -1,6 +1,7 @@
 package be.uantwerpen.fti.ei.game;
 
 import be.uantwerpen.fti.ei.Input;
+import be.uantwerpen.fti.ei.visuals.J2DCollectable;
 import be.uantwerpen.fti.ei.visuals.J2DObstacle;
 
 import java.util.Arrays;
@@ -12,6 +13,7 @@ public class Game {
     private boolean paused;
     private final AFact af;
     private AObstacle obstacle;
+    private ACollectable collectable;
     private Input input;
     private int cellsX = 25;
     private int cellsY = 15;
@@ -22,10 +24,15 @@ public class Game {
             1,12,2,12,3,12,4,12,5,12
     ));
 
+    private final Map<Integer, Integer> collectables = new HashMap<>(Map.of(
+            2,10,4,11
+    ));
+
     public Game(AFact af){
         this.af = af;
         this.input = new Input(af.getGctx());
         this.obstacle = new J2DObstacle(blocks, af.getGctx());
+        this.collectable = new J2DCollectable(collectables, af.getGctx());
     }
 
     public void run(){
@@ -44,6 +51,7 @@ public class Game {
             }
             if (!paused) {
                 obstacle.vis();
+                collectable.vis();
                 af.getGctx().render();
             }
             try{
